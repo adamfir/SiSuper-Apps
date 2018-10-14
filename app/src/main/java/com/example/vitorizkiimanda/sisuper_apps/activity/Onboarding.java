36afa8293261f6ed83ec7d796a28b5a57db48f19
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-10-14 13:22:49 +0700

    Onboarding pages : indicator

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/Onboarding.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/Onboarding.java
@@ -1,13 +1,80 @@
 package com.example.vitorizkiimanda.sisuper_apps.activity;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.vitorizkiimanda.sisuper_apps.R;
+import com.example.vitorizkiimanda.sisuper_apps.provider.OnboardingAdapter;
 
 public class Onboarding extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotsLayout;
+
+    private TextView[] mDots;
+
+    private OnboardingAdapter onboardingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        mSlideViewPager = findViewById(R.id.slideViewPager);
+        mDotsLayout = findViewById(R.id.dotsLayout);
+
+        onboardingAdapter = new OnboardingAdapter(this);
+
+        mSlideViewPager.setAdapter(onboardingAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotsIndicator(int position){
+        mDots = new TextView[5];
+
+        for (int i = 0 ; i < mDots.length ; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorPurplDark));
+
+            mDotsLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+    public void changeDotsIndicator(int position){
+        for (int i = 0 ; i < mDots.length ; i++){
+            if(position == i) mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+            else mDots[i].setTextColor(getResources().getColor(R.color.colorPurplDark));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            changeDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/provider/OnboardingAdapter.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/provider/OnboardingAdapter.java
@@ -1,4 +1,83 @@
 package com.example.vitorizkiimanda.sisuper_apps.provider;
 
-public class OnboardingAdapter {
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.vitorizkiimanda.sisuper_apps.R;
+
+public class OnboardingAdapter extends PagerAdapter{
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public OnboardingAdapter(Context context){
+        this.context = context;
+    }
+
+    //Arrays
+    public int[] onboarding_images = {
+            R.drawable.maskot,
+            R.drawable.onboard2,
+            R.drawable.onboard3,
+            R.drawable.onboard4,
+            R.drawable.onboard5
+    };
+
+    public String[] onboarding_headings = {
+            "Sistem Informasi Sinkronasi",
+            "Kegiatan",
+            "Undangan",
+            "Pajak & Zakat",
+            "Daftar Harga"
+    };
+
+    public String[] onboarding_description = {
+            "UMKM-Pemerintah-Rakyat",
+            "Temukan Informasi Kegiatan Terkini dari Pemerintah",
+            "Dapatkan Undangan Pelatihan dari Pemerintah",
+            "Ketahui Jumlah Pajak dan Zakat Terkini dari Usaha",
+            "Cek Harga Bahan Baku Terkini dan Buat Daftar Sesuai Kebutuhan"
+    };
+
+    //Arrays ^^^
+
+    @Override
+    public int getCount() {
+        return onboarding_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.onboarding_item, container, false);
+
+        ImageView onboardingImangeVIew =  view.findViewById(R.id.onboarding_image);
+        TextView onboardingHeading = view.findViewById(R.id.onboarding_heading);
+        TextView onboardingDescription = view.findViewById(R.id.onboarding_description);
+
+        onboardingImangeVIew.setImageResource(onboarding_images[position]);
+        onboardingHeading.setText(onboarding_headings[position]);
+        onboardingDescription.setText(onboarding_description[position]);
+
+        container.addView(view);
+
+        return view;
+    };
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
 }
