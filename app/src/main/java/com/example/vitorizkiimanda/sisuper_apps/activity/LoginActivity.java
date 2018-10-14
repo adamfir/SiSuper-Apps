da72f95b88ce720c144e48bc205ce4c72d1ef71a
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-10-14 22:15:11 +0700

    UI revision

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/LoginActivity.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.vitorizkiimanda.sisuper_apps.activity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -101,6 +103,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+        Button forgotPasswordButton = (Button) findViewById(R.id.forgot_password);
+        forgotPasswordButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forgotPassword();
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
@@ -157,6 +167,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private void toRegister(){
         Intent moveIntent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(moveIntent);
+    }
+
+    private void forgotPassword(){
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("Under Maintenance")
+        .setMessage("this feature is currently under maintenance")
+        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // continue with delete
+            }
+        })
+//        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//                // do nothing
+//            }
+//        })
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .show();
     }
 
     private void attemptLogin() {
