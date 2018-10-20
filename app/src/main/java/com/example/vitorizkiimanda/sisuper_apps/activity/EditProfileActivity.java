05c945cc06171af5cd29e82493a5dfe87d286c25
ryanazrian <azrian.ryan@gmail.com>
2018-10-20 15:09:08 +0700

    change profile picture

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProfileActivity.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProfileActivity.java
@@ -1,19 +1,39 @@
 package com.example.vitorizkiimanda.sisuper_apps.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.vitorizkiimanda.sisuper_apps.R;
 import com.example.vitorizkiimanda.sisuper_apps.fragment.ProductListFragment;
 
 public class EditProfileActivity extends AppCompatActivity {
-
+    private ImageView photoProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        //change photo profile
+        photoProfile = findViewById(R.id.photo_profile);
+        photoProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        photoProfile.setImageBitmap(bitmap);
 
     }
 }
