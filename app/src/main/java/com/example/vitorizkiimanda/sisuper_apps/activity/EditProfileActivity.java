ac97fea6d484ef7f0ea511bcf8d43d7b19ad0de2
ryanazrian <azrian.ryan@gmail.com>
2018-11-04 19:18:47 +0700

    Adding method to upload pick image -- EditProfile Activity

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProfileActivity.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProfileActivity.java
@@ -1,8 +1,13 @@
 package com.example.vitorizkiimanda.sisuper_apps.activity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +18,7 @@ import com.example.vitorizkiimanda.sisuper_apps.fragment.ProductListFragment;
 
 public class EditProfileActivity extends AppCompatActivity {
     private ImageView photoProfile;
+    Integer REQUEST_CAMERA = 1, SELECT_FILE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +29,10 @@ public class EditProfileActivity extends AppCompatActivity {
         photoProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+             //   Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+             //   startActivityForResult(intent, 0);
+
+                SelectImage();
             }
         });
     }
@@ -32,8 +40,42 @@ public class EditProfileActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        photoProfile.setImageBitmap(bitmap);
 
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == REQUEST_CAMERA){
+                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+                photoProfile.setImageBitmap(bitmap);
+            }
+            else if(requestCode == SELECT_FILE){
+                Uri selectedImage = data.getData();
+                photoProfile.setImageURI(selectedImage);
+            }
+        }
+
+    }
+
+    private void SelectImage(){
+        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditProfileActivity.this);
+        builder.setTitle("Select Method");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(items[i].equals("Camera")){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }
+                else if (items[i].equals("Gallery")){
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+                }
+                else if (items[i].equals("Cancel")){
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 }
