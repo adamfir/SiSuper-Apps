18df52716c0e85438514e3dc58f9622c1b6518ce
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-16 12:06:19 +0700

    event detail UI : button added,  DB : initialized

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EventDetail.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EventDetail.java
@@ -40,6 +40,6 @@ public class EventDetail extends AppCompatActivity {
         eventOrganizer.setText(model.getOrganized());
         eventDate.setText(model.getDate());
         eventLocation.setText(model.getEventPlace());
-        eventDescription.setText(model.getDescription());
+        eventDescription.setText(model.getDescription()+"as;lkfjas;lkjf l;kasdflk ;asjdflk sal;kf jsal;kfj ;lksaklf jsalkjf lk;sad;fl jasl;kf jl;ask jfjas dl;fjs;lk lksd f;lkjasldk jl;sdakj ;lskdj fksajdl;fk jaslkd jflk;saj dfl;k sda;lkfj ;lskadfj ;laskdjf ;lsjd ;lkjsdlkfj sldfj ;sldjf;l jsa;ld kj;saldkj fl;sadkj f;slakj d;ls kdjslkdf j;sla lksaj d;lsd lkajs d;lksjda ;lksj lksdaj f;lksdj ;lsdkj f;aslkd jfas;ldkf jas; lkjsfda ;lks dj;salkd j;salk fjs;ld jkfas;kldjfsd;lk ja;sdlk jsd;lfj as;ldfj a;lsdjf ;asldjf ;sdljkf lksajdf;lkjsa;dlfk j;lsdkf j;lksadjf ;lksjad; flsdlk; fls;dj f;ls jd;sdfsdsd f sdf sf sad f sa fsd f ds f sa f asd f asd f sdf  sdf sd f s df dsfs df s df sa df asdf as dfsdf sd fsd f sdfsadf sa fd dasf sa df sd f sd f sdf safs adf asf sad f sfdajsdkljsdalk;f jlksdj flksdl;kfjs;ld kj;lsj l;ksdlfk jalskf lk;asd;lfk as;lkfj asd");
     }
 }
--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/database/AgendaEvent.kt
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/database/AgendaEvent.kt
@@ -1,2 +1,24 @@
 package com.example.vitorizkiimanda.sisuper_apps.database
 
+data class AgendaEvent(
+        val id: Int?,
+        val eventId: String?,
+        val eventImage: String?,
+        val eventDate: String?,
+        val eventName: String?,
+        val eventPlace: String?,
+        val eventOrganizer: String?,
+        val eventDescription: String?) {
+
+    companion object {
+        const val TABLE_AGENDA_EVENT: String = "TABLE_AGENDA_EVENT"
+        const val ID: String = "ID_"
+        const val EVENT_ID: String = "EVENT_ID"
+        const val EVENT_IMAGE: String = "EVENT_IMAGE"
+        const val EVENT_DATE: String = "EVENT_DATE"
+        const val EVENT_NAME: String = "EVENT_NAME"
+        const val EVENT_PLACE: String = "EVENT_PLACE"
+        const val EVENT_ORGANIZER: String = "EVENT_ORGANIZER"
+        const val EVENT_DESCRIPTION: String = "EVENT_DESCRIPTION"
+    }
+}
