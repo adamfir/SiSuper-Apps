0b4bc4c745011e4ec1d5fa1f668f1017436e6f62
ryanazrian <azrian.ryan@gmail.com>
2018-12-16 19:47:21 +0700

    Product List

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/fragment/ProductListFragment.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/fragment/ProductListFragment.java
@@ -1,33 +1,67 @@
 package com.example.vitorizkiimanda.sisuper_apps.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.vitorizkiimanda.sisuper_apps.R;
 import com.example.vitorizkiimanda.sisuper_apps.activity.EditProduct;
 import com.example.vitorizkiimanda.sisuper_apps.activity.LoginActivity;
 import com.example.vitorizkiimanda.sisuper_apps.activity.Onboarding;
 import com.example.vitorizkiimanda.sisuper_apps.activity.ProductInput;
+import com.example.vitorizkiimanda.sisuper_apps.provider.EndPoints;
+import com.example.vitorizkiimanda.sisuper_apps.provider.SessionManagement;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductListFragment extends Fragment {
+    Context mContext;
+    SessionManagement session;
+
+    String token;
+    String businessID;
 
 
     public ProductListFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(final Activity activity) {
+        super.onAttach(activity);
+        mContext = activity;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //session
+        session = new SessionManagement(mContext);
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product_list, container, false);
         View addProductButton = view.findViewById(R.id.button_add_product);
@@ -48,7 +82,66 @@ public class ProductListFragment extends Fragment {
             }
         });
 
+        getData();
+
         return view;
+    }
+
+    public void getData(){
+        HashMap userProfile = session.getUserDetails();
+        HashMap businessProfile = session.getBusiness();
+
+        token = (String) userProfile.get("token");
+        businessID = (String) businessProfile.get("business");
+        final String url = EndPoints.ROOT_URL+"/products/getProductByBussinessId";
+
+        StringRequest postRequest  =  new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject result = new JSONObject(response);
+                            JSONArray results = result.getJSONArray("result");
+                            System.out.println(results);
+
+                            Toast.makeText(mContext, "Retrieve Produk Berhasil", Toast.LENGTH_LONG).show();
+//                            showProgress(false);
+//                            finish();
+
+                        } catch (JSONException e) {
+                            Toast.makeText(mContext, "Internal Server Error", Toast.LENGTH_LONG).show();
+//                            showProgress(false);
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(mContext, "Internal Server Error", Toast.LENGTH_LONG).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("businessId", businessID);
+
+                return params;
+            }
+
+            /** Passing some request headers* */
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        requestQueue.add(postRequest);
     }
 
 
