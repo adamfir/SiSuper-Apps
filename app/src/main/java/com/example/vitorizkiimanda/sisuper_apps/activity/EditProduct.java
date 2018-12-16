8f2912f20276b99d481f5402893000f5366dda20
ryanazrian <azrian.ryan@gmail.com>
2018-12-16 15:20:44 +0700

    deleting space

--- app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProduct.java
+++ app/src/main/java/com/example/vitorizkiimanda/sisuper_apps/activity/EditProduct.java
@@ -85,8 +85,6 @@ public class EditProduct extends AppCompatActivity implements AdapterView.OnItem
 
         if(cancel){
             focusView.requestFocus();
-
-
         }
         else {
             Log.d("nama", productName);
